0c8fd96e7213ee0604a1791920f8ec177527eded
pt <162039036+chnlr@users.noreply.github.com>
2024-05-20 20:48:59 -0500

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -8,10 +8,13 @@ document.getElementById('doorImage').addEventListener('click', function() {
     gifPreload.onload = function() {
         setTimeout(function() {
             door.style.display = 'none'; // Optionally hide the door image
+            var companyName = document.getElementById('companyName');
+            companyName.style.visibility = 'visible'; // Make the company name visible
+            companyName.style.opacity = 1; // Start the fade-in effect for company name
             setTimeout(function() {
                 var comingSoon = document.getElementById('comingSoon');
                 comingSoon.style.visibility = 'visible'; // Make the 'coming soon' text visible
-                comingSoon.style.opacity = 1; // Start the fade-in effect
+                comingSoon.style.opacity = 1; // Start the fade-in effect for 'coming soon'
             }, 2000); // Delay for 'coming soon' to start fading in after 'Easy Storage Solutions' appears
         }, 1000); // Adjust this time to match the duration of your GIF
     };
@@ -19,12 +22,15 @@ document.getElementById('doorImage').addEventListener('click', function() {
     if (gifPreload.complete) {
         door.src = gifPreload.src;
         setTimeout(function() {
-            door.style.display = 'none'; // Optionally hide the door image
+            door.style.display = 'none';
+            var companyName = document.getElementById('companyName');
+            companyName.style.visibility = 'visible';
+            companyName.style.opacity = 1;
             setTimeout(function() {
                 var comingSoon = document.getElementById('comingSoon');
-                comingSoon.style.visibility = 'visible'; // Make the 'coming soon' text visible
-                comingSoon.style.opacity = 1; // Start the fade-in effect
-            }, 2000); // Delay for 'coming soon' to start fading in after 'Easy Storage Solutions' appears
+                comingSoon.style.visibility = 'visible';
+                comingSoon.style.opacity = 1;
+            }, 2000);
         }, 1000);
     }
 });
