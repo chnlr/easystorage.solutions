fcbd2f51d413b3bf0f47852f9ff66759b8ae1803
pt <162039036+chnlr@users.noreply.github.com>
2024-05-28 06:49:03 -0500

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,32 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     const doorImage = document.getElementById('doorImage');
+    const doorVideo = document.getElementById('doorVideo');
     const companyName = document.getElementById('companyName');
     const comingSoon = document.getElementById('comingSoon');
     const banner = document.getElementById('banner');
-    const gifPreload = new Image();
-    gifPreload.src = 'doorfall.gif';
 
     doorImage.addEventListener('click', function() {
-        if (!gifPreload.complete) { // Checks if the GIF is not yet loaded
-            gifPreload.onload = showText; // Binds function to onload if GIF hasn't loaded
-        } else {
-            showText(); // Executes immediately if GIF is already loaded
-        }
-        doorImage.src = gifPreload.src; // Starts playing the GIF
+        doorImage.style.display = 'none';
+        doorVideo.style.display = 'block';
+        doorVideo.play();
+    });
+
+    doorVideo.addEventListener('ended', function() {
+        showText();
+        doorVideo.style.display = 'none';
     });
 
     function showText() {
         setTimeout(function() {
-            doorImage.style.display = 'none'; // Hide the GIF after playing
             companyName.style.visibility = 'visible';
-            companyName.style.opacity = 1; // Fade in the company name immediately after GIF
+            companyName.style.opacity = 1;
             setTimeout(function() {
                 comingSoon.style.visibility = 'visible';
-                comingSoon.style.opacity = 1; // Fade in 'coming soon' shortly after
+                comingSoon.style.opacity = 1;
                 setTimeout(function() {
-                    banner.style.top = '0'; // Slide down the banner
-                }, 1600); // Delay for banner to appear after 'coming soon'
-            }, 200); // Minimal delay for 'coming soon' to appear after company name
-        }, 800); // Assumed GIF duration before text appears
+                    banner.style.top = '0';
+                }, 1600);
+            }, 200);
+        }, 800);
     }
 });
